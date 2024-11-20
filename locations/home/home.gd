33fb3c0b50b27f97e89e38dff6cc390c8695b88a
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-11-20 20:15:11 +0200

    Additional writing

--- locations/home/home.gd
+++ locations/home/home.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal_event)
 	Dialogic.start("intro_timeline")
+
+func _on_dialogic_signal_event(argument: String) -> void:
+	if argument == "quit_game":
+		get_tree().quit()
